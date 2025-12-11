5cac444b38c81e9695761490ef65c9a9c682b6b9
muhammaduamrsaleem <u2025646@giki.edu.pk>
2025-12-11 22:12:50 +0500

    Update 2025411_2_1.cpp

--- 2025411_2_1.cpp
+++ 2025411_2_1.cpp
@@ -1,21 +1,3 @@
-#include <iostream>
-using namespace std;
 
-int main()
-{
 
-    int number, a;
-    cout << "Enter a number: ";
-    cin >> number;
-
-    while (number != 0)
-    {
-        a = number / 10;
-        a = a * 10;
-        a = number - a;
-        cout << a;
-        
-    }
-    return 0;
-}
 
